8c67dc8c3cfcde934013fcbd2230358cdd46639e
vince <vinceparis19@gmail.com>
2019-08-01 09:36:01 -0400

    8.1 added a simple bell pair circuit

---
+++ quantumComputing/simpleOps/qrng_a.py
@@ -0,0 +1,24 @@
+import numpy as np
+from qiskit import (QuantumCircuit, execute, Aer)
+from qiskit.visualization import plot_histogram
+
+simulator = Aer.get_backend('qasm_simulator')
+
+circuit = QuantumCircuit(2,2)
+
+circuit.h(0)
+
+circuit.cx(0,1)
+
+circuit.measure([0,1],[0,1])
+
+job = execute(circuit, simulator, shots=1000)
+
+result = job.result()
+
+counts = result.get_counts(circuit)
+print("/nTotal count for 00 and 01 are: ", counts)
+
+circuit.draw(output='mpl')
+
+
