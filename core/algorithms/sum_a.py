1cd918b86be191473a198454303d1aa3cee00268
vince <vinceparis19@gmail.com>
2019-08-03 01:40:12 -0400

    added a core algorithm for summing numbers in an algoritms folder, and a core parent folder and pandas sister folder

---
+++ core/algorithms/sum_a.py
@@ -0,0 +1,13 @@
+# sum numbers in a sequence
+# takes in a number and adds each number in the range
+# i.e. 4 = 1 + 2 + 3 + 4 = 10
+
+
+def sum_a(num):
+    return sum(range(num + 1))
+
+
+print(sum_a(4))
+
+
+
