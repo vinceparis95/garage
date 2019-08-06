4186516dc657e00e579c307724f1f74ccbfdc165
vince <vinceparis19@gmail.com>
2019-08-05 20:15:35 -0400

    added a numpy sort method

--- core/algorithms/sorting/numpySort.py
+++ core/algorithms/sorting/numpySort.py
@@ -1,9 +1,12 @@
 import numpy as np
 
+# create a python list
 pythonList = [1, 9, 19, 5, 361, 95]
 
+# convert python list to numpy array
 numpyArray = np.asarray(pythonList)
 
+# sort the numpy array with sort method
 sortedNumpyArray = np.sort(numpyArray)
 
 print(sortedNumpyArray)
