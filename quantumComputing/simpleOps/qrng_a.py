561a6f0d10841b6b8a626d1e7428d546c34e6b7f
vince <vinceparis19@gmail.com>
2019-08-07 00:00:35 -0400

    added a simple quantum random number generator

--- quantumComputing/simpleOps/qrng_a.py
+++ quantumComputing/simpleOps/qrng_a.py
@@ -1,2 +1,25 @@
-import qiskit
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, execute, Aer, IBMQ, BasicAer
+import math
+## Uncomment the next line to see diagrams when running in a notebook
+#%matplotlib inline
 
+## Example 2-1: Random bit
+# Set up the program
+reg = QuantumRegister(1, name='reg')
+reg_c = ClassicalRegister(1, name='regc')
+qc = QuantumCircuit(reg, reg_c)
+
+qc.reset(reg)          # write the value 0
+qc.h(reg)              # put it into a superposition of 0 and 1
+qc.measure(reg, reg_c) # read the result as a digital bit
+
+backend = BasicAer.get_backend('statevector_simulator')
+job = execute(qc, backend)
+result = job.result()
+
+counts = result.get_counts(qc)
+print('counts:',counts)
+
+outputstate = result.get_statevector(qc, decimals=3)
+print(outputstate)
+qc.draw()        # draw the circuit
