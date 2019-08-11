5091ae448f4d218768caa002a8fdf296893d1a8f
vince <vinceparis19@gmail.com>
2019-08-11 01:53:18 -0400

    added a few more structuring modifications again

---
+++ machineLearning/coreFrameworks/pandasBox/numpy_converter_a.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import numpyBox as np
+
+numpyArray = np.array([[1, 2, 3], [3, 2, 1]])
+print(numpyArray.shape)
+
+df = pd.DataFrame(numpyArray)
+
+print(df)
