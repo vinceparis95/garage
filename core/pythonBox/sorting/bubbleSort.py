690964f79a83e3a277e7d9e509a426db763fa824
vince <vinceparis19@gmail.com>
2019-08-10 22:51:19 -0400

    restructured core around boxes

--- core/pythonBox/sorting/bubbleSort.py
+++ core/pythonBox/sorting/bubbleSort.py
@@ -1,5 +1,5 @@
 def bubbleSort(list):
-    for iter_num in range(len(list)-1,0,-1):
+    for iter_num in range(len(list)-1, 0, -1):
         for idx in range(iter_num):
             if list[idx]> list[idx+1]:
                 temp = list[idx]
