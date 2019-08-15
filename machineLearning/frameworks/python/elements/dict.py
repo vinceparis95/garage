24487cc54cf1671f79345bf63acd2ab4283e77ad
vince <vinceparis19@gmail.com>
2019-08-15 11:24:22 -0400

    added core python elements like list and dict

---
+++ machineLearning/frameworks/python/elements/dict.py
@@ -0,0 +1,16 @@
+
+# create a dict
+Dict = {}
+print(Dict)
+
+# create a dict with integer keys
+Dict = {1: "plant computer", 2: "soft drone"}
+print(Dict)
+
+# create a dict with a dict method
+Dict = dict({1: "pco1", 2: "dr01"})
+print(Dict)
+
+# with each item in a pair
+Dict = dict([(1, "plant breeding"), (2, "soft bots")])
+print(Dict)
---
+++ machineLearning/frameworks/python/elements/list.py
@@ -0,0 +1,18 @@
+
+# create a list
+List = []
+print(List)
+
+# create a populated list
+List = [1, 5, 9]
+print(List)
+
+# print elements 0 and 2
+# (first and third item)
+print(List[0], List[2])
+
+# add elements to the list
+List.append(95)
+print(List)
+List.append(361)
+print(List)
