dcbf8809128fb7be15b299aa5967839474615955
vince <vinceparis19@gmail.com>
2019-08-18 12:09:24 -0400

    made minor edits to estimator and a regressor file

--- machineLearning/instruments/regressors/linearRegressors/linearRegressor_b.py
+++ machineLearning/instruments/regressors/linearRegressors/linearRegressor_b.py
@@ -1,15 +1,9 @@
-
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import KFold
-from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Dense
-from tensorflow.python.keras.wrappers.scikit_learn import KerasRegressor
 
 import os
 dataset = np.loadtxt('/home/vince/Desktop/pima.csv', delimiter=",")
@@ -40,19 +34,19 @@ history = model.fit(X_train, y_train,
                     verbose=1,
                     validation_split=0.2)
 
-# graph loss in pyplot
-# print(history.history.keys())
-# plt.plot(history.history['loss'])
-# plt.plot(history.history['val_loss'])
-# plt.title('model loss')
-# plt.ylabel('loss')
-# plt.xlabel('epoch')
-# plt.legend(['train', 'validation'], loc='upper left')
-# plt.show()
+print(history.history.keys())
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'validation'], loc='upper left')
+plt.show()
 
 Xnew = np.array([[3,100,44,75,30.9]])
 Xnew= scaler_x.transform(Xnew)
 ynew= model.predict(Xnew)
+
 #invert normalize
 ynew = scaler_y.inverse_transform(ynew)
 Xnew = scaler_x.inverse_transform(Xnew)
