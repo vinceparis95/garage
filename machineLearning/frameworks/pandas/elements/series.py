51f1e78d819c4e566841e90f8032e49e59a32634
vince <vinceparis19@gmail.com>
2019-08-18 00:44:28 -0400

    added a series file to pandas ops

---
+++ machineLearning/frameworks/pandas/elements/series.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+
+# # create a series from a numpy array
+# array = np.array([1, 5, 9])
+# series = pd.Series(array)
+#
+# # print the series
+# print(series)
+#
+# # print the nth item
+# print(series[1])
+
+##########################################
+
+# create a series from a column of csv data
+csv = pd.read_csv("/home/vince/Desktop/youthy.csv")
+csv_series = pd.Series(csv['Age'])
+csv_series_b = pd.Series(csv['Neighborhood'])
+# print(csv_series)
+# print(csv_series_b)
+print(csv_series_b.head(5))
+
+###########################################
+
