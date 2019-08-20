0427526f2d4012f33229d22ea2edf8844d70cc59
vince <vinceparis19@gmail.com>
2019-08-20 00:15:32 -0400

    modified python functions

--- machineLearning/frameworks/python/operations/functions/filter.py
+++ machineLearning/frameworks/python/operations/functions/filter.py
@@ -1,7 +1,7 @@
 # Filtering functions in python
 
 # create a list of components
-component = ['plant computer', 'soft drones', 'web service']
+component = ['plant computer', 'soft drones', 'tf_js service']
 
 # filter the list with a value
 filtered = filter(lambda x: x == 'soft drones', component)
--- machineLearning/frameworks/python/operations/functions/map.py
+++ machineLearning/frameworks/python/operations/functions/map.py
@@ -1,7 +1,7 @@
 # Mapping functions in python
 
 # create a python list
-component = ['plant computer', 'soft drones', 'web service']
+component = ['plant computer', 'soft drones', 'tf_js service']
 
 # map the components
 mapped = map(lambda x: 'component: ' + x, component)
