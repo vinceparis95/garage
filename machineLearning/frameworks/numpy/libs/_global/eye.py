45477b19113fb9ea8066b1eaff84744d3c391e71
vince <vinceparis19@gmail.com>
2019-08-21 10:33:50 -0400

    added to numpy libs

---
+++ machineLearning/frameworks/numpy/libs/_global/eye.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+# identity matrix
+ayeaye = np.eye(9)
+
+print(ayeaye)
---
+++ machineLearning/frameworks/numpy/libs/_global/random.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+np.random.seed(0)
+
+x = np.random.randint(10, size=(3,3,3))
+
+print(x)
